80a4b19cd5bd8fab2372746cf6f4d13e487fabcd
KeenanAkit <keenanakit@gmail.com>
2017-10-24 17:47:17 -0400

    Initial Commit

---
+++ Ensemble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ensemble
+//
+//  Created by Keenan Akit on 2017-10-24.
+//  Copyright © 2017 Keenan Akit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
